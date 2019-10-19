6f1f515aa43d460655ef348862c0df861e0dfa3e
Daniil <dan28012000@gmail.com>
2019-10-20 00:08:13 +0500

    add back

--- src/pages/AuthForm.js
+++ src/pages/AuthForm.js
@@ -3,22 +3,33 @@ import Paper from '@material-ui/core/Paper';
 import CustomButton from '../components/button';
 import CustomTextField from '../components/textField';
 import '../css/auth-form.css';
-// import Axios from 'axios';
+import axios from 'axios';
 
 export default class AuthForm extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       regField: '',
+      login: '',
+      password: '',
 
     }
   }
-  handleChange = () => {
-    alert('asas')
+  handleChange = (name,value) => {  
+    this.setState({[name]:value})
+    console.log(value)
   }
   onClickButton = () => {
-    
+    let data = {
+      login: this.state.login,
+      password: this.state.password
+    };
+    axios.post('http://172.20.10.2/api/auth/', {data} )
+      .then(res => {
+        console.log(res);
+      })
   }
+
   componentDidMount(){
     
   }
@@ -33,14 +44,17 @@ render(){
         <CustomTextField 
             label='Телефон' 
             class='registration_form__field' 
-            val={this.state.regField} 
+            val={this.state.login} 
             handleChange={this.handleChange} 
+            name = {'login'}
         />
         <CustomTextField 
             label='Пароль' 
             class='registration_form__field' 
-            val={this.state.regField} 
+            val={this.state.password} 
             handleChange={this.handleChange} 
+            name={'password'}
+            type = {'password'}
         />
         <CustomButton buttonVal='Войти' onClickBtn={this.onClickButton}/>
       </form>
