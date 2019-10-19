dea01765f3ef34122383ac0761a458c9b9c29e21
Pakvakun <zhitnikov.96@mail.ru>
2019-10-19 15:15:42 +0500

    mrg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Button from './components/button';
-import './App.css';
+import './css/App.css';
+import Axios from 'axios';
 
 export default class App extends React.Component{
   constructor(props){
@@ -16,38 +14,15 @@ export default class App extends React.Component{
     alert('asas')
   }
   onClickButton = () => {
-    alert('button clicjed')
+
+  }
+  componentDidMount(){
+    
   }
 render(){
   return (
     <div className='SPA__Motiv'>
-      <Paper 
-        className='registration_form'
-        elevation={5}
-        square={false}
-      >
-      <form  noValidate autoComplete="off">
-        <TextField
-          id="outlined-name"
-          label="Телефон"
-          className='registration_form__field'
-          value={this.state.regField}
-          onChange={(value) => this.handleChange(value)}
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-name"
-          label="Пароль"
-          className='registration_form__field'
-          value={this.state.regField}
-          onChange={(value) => this.handleChange(value)}
-          margin="normal"
-          variant="outlined"
-        />
-        <Button buttonVal='Войти' onClickBtn={this.onClickButton}/>
-      </form>
-      </Paper>
+      
     </div>
 )}
 }
--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 
 // import './button.css';
 
-export default class App extends React.Component{
+export default class Button extends React.Component{
   constructor(props){
     super(props);
     this.state = {
